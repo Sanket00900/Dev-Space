eb87e910899ecbfd0ab5e9d709a923b3b17e4860
Sanket_Jagdale <sanketsjagdale99@gmail.com>
2022-10-08 15:31:18 +0530

    routes added

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+//define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
