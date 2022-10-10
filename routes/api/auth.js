23f2933fab490e705dceba5fa8466e9e435c9644
Sanket Jagdale <sanketsj00900@gmail.com>
2022-10-10 10:30:01 +0530

    updated authorzation

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -9,7 +9,7 @@ const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Test route
-// @access  Public
+// @access  Private
 
 router.get("/", auth, async (req, res) => {
   try {
