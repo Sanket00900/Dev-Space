3441252fe18a355e196835ad0134efda59ecbc13
Sanket Jagdale <sanketsj00900@gmail.com>
2022-10-10 10:29:36 +0530

    updated middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,6 +16,6 @@ module.exports = function (req, res, next) {
     req.user = decoded.user;
     next();
   } catch (err) {
-    res.status(401).son({ msg: "Token is not valid" });
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
