0477a4a6c6fd8ded281e1fe3cc34328b8c702f22
Sanket Jagdale <sanketsj00900@gmail.com>
2022-10-24 16:51:57 +0530

    added delete comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -130,7 +130,7 @@ router.put("/like/:id", auth, checkObjectId("id"), async (req, res) => {
   }
 });
 
-//* @route    api/posts/unlike/:id
+//* @route   api/posts/unlike/:id
 //? @desc    unlike a post
 //! @access  Private
 
@@ -197,5 +197,39 @@ router.post(
     }
   }
 );
+
+//? @route    DELETE api/posts/comment/:id/:comment_id
+//* @desc     Delete comment
+//! @access   Private
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
+    await post.save();
+
+    return res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
